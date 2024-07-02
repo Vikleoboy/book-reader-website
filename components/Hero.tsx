45e927ba7e9faf9eeafaac230d218a6cf1ca7798
Vikleo <vikleoboy@gmail.com>
2024-07-02 16:20:06 +0530

    made some changes

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -7,17 +7,26 @@ import { FaWindows } from "react-icons/fa";
 
 const Hero = () => {
   return (
-    <main className=" bg-neutral-200/60 flex flex-col justify-center items-center pt-16 sm:pt-24 relative">
-      <Image src={iconBook} alt="book icon" quality={100} className="max-small:w-[35px]" />
-      <p className="text-black  leading-relaxed text-xl sm:text-2xl lg:text-3xl mt-2 sm:mt-3 lg:mt-4">Axiom Books</p>
+    <main className=" bg-neutral-200/80 flex flex-col justify-center items-center pt-16 sm:pt-24 relative">
+      <Image
+        src={iconBook}
+        alt="book icon"
+        quality={100}
+        className="max-small:w-[35px]"
+      />
+      <p className="text-black  leading-relaxed text-xl sm:text-2xl lg:text-3xl mt-2 sm:mt-3 lg:mt-4">
+        Axiom Books
+      </p>
       <h1 className="max-xs:text-[1.7rem] text-[2rem] small:text-[2rem] sm:text-[3.5rem] navsize:text-6xl  lg:text-7xl desktop:text-[80px] text-center   font-bold leading-tight sm:leading-[1.275] navsize:leading-[80px] lg:leading-[95px] pt-1">
         Explore, discover
         <span className="block font-bold">— all in one read.</span>
       </h1>
       <div className="hero-reverse flex flex-col justify-center items-center ">
         <p className="text-lg small:text-xl sm:text-2xl largesceen:text-3xl text-center text-gray-800 font-normal leading-normal w-[85%] sm:w-[70%] lg:w-[60%] largesceen:w-[38vw] mt-5 sm:mt-10">
-          Axiom Books offers a comprehensive library of your favorite reads and exciting new discoveries—all in one convenient platform. Browse our curated selection to find your next favorite book,
-          track your reading journey, and set personal reading goals effortlessly.
+          Axiom Books offers a comprehensive library of your favorite reads and
+          exciting new discoveries—all in one convenient platform. Browse our
+          curated selection to find your next favorite book, track your reading
+          journey, and set personal reading goals effortlessly.
         </p>
 
         <div className="flex max-xs:flex-col items-center justify-center gap-6 xs:gap-2 md:gap-8 small:text-base text-xs desktop:text-lg">
@@ -32,7 +41,13 @@ const Hero = () => {
           </button>
         </div>
 
-        <Image src={library1} alt="library page review" placeholder="blur" quality={100} className="w-[80%] md:mt-14 desktop:mt-20 rounded-t-3xl" />
+        <Image
+          src={library1}
+          alt="library page review"
+          placeholder="blur"
+          quality={100}
+          className="w-[80%] md:mt-14 desktop:mt-20 rounded-t-3xl"
+        />
       </div>
     </main>
   );
